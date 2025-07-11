e90a299b2676298032147322b4bb411c55a7831c
Eugen <eskhmelev@gmail.com>
2025-07-11 09:32:35 +0200

    20250711-0932

---
+++ src/components/pages/About/About.tsx
@@ -0,0 +1,12 @@
+import style from "./About.module.css";
+
+function About() {
+  return (
+    <div className={style.container}>
+      <p>
+        <h2 className={style.title}>О нас</h2>
+        </p>
+    </div>
+  );
+}
+export default About;
---
+++ src/components/pages/Contact/Contact.tsx
@@ -0,0 +1,9 @@
+import style from "./Contact.module.css"
+
+export default function Contact() {
+  return (
+    <div className={style.container}>
+      <h2 className={style.title}>Contact</h2>
+    </div>
+  );
+}
